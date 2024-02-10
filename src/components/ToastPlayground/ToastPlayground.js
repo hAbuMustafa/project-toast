a10e4efe8301855448c9c13ff8808f9dbc5f1d5c
hosam5553 <hosam5553@gmail.com>
2024-02-10 10:56:00 +0000

    done exercise 2

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -8,11 +9,20 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = useState("");
+  const [toastVisible, toggleToastVisibility] = useToggle(false);
   const [selectedVariant, setSelectedVariant] = useState(VARIANT_OPTIONS[0]);
 
   return (
     <div className={styles.wrapper}>
       <Header />
+
+      {toastVisible && (
+        <Toast
+          variant={selectedVariant}
+          message={message}
+          dismiss={toggleToastVisibility}
+        />
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -61,7 +71,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={toggleToastVisibility}>Pop Toast!</Button>
           </div>
         </div>
       </div>
@@ -76,6 +86,16 @@ function Header() {
       <h1>Toast Playground</h1>
     </header>
   );
+}
+
+function useToggle(defaultState) {
+  const [value, setValue] = useState(defaultState);
+
+  function toggleState() {
+    setValue((currentValue) => !currentValue);
+  }
+
+  return [value, toggleState];
 }
 
 export default ToastPlayground;
